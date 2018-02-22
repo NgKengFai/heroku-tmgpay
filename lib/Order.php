94f1ea0dbc144f968f2cacc54163a2d65e0b8255
LAPTOP-D6G4E15I\User <kf@tamago.live>
2018-02-23 05:10:58 +0800

    changes to session id

--- lib/Order.php
+++ lib/Order.php
@@ -41,14 +41,15 @@ class Order
 	
 	public function init(){
 		try{
-			$this->value = $_COOKIE['value']*100;
+			$this->value = $_SESSION['value']*100;
 			$this->amount['value'] = $this->value;
-			$this->currencyCode = $_COOKIE["currency"];
+			$this->currencyCode = $_SESSION["currency"];
 			$this->amount['currency'] = $this->currencyCode;
-			$this->reference = $_COOKIE["order_id"];
+			$this->reference = date("Ymdhis").mt_rand(1000,9999);
+			//$this->reference = $_COOKIE["order_id"];
 			//$this->shopper.telephoneNumber = $_COOKIE["phonenumber"];
-			$this->shopperReference = $_COOKIE["phonenumber"];
-			$this->countryCode = $_COOKIE["countrycode"];
+			$this->shopperReference = $_SESSION["phonenumber"];
+			$this->countryCode = $_SESSION["countrycode"];
 			
 		}
 		catch(Exception $e){
