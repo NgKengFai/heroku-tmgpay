7c72ef7a2330cee8bba017f0b6e9a6ff959e4f4f
NgKengFai <34302683+NgKengFai@users.noreply.github.com>
2018-02-22 13:13:32 +0800

    Add files via upload

--- lib/Client.php
+++ lib/Client.php
@@ -40,7 +40,7 @@ class Client
             'shopperReference' => $order->getShopperReference(),
             'shopperLocale' => $order->getShopperLocale(),
             'reference' => $order->getReference(),
-			'shopper.telephoneNumber'=> $order->getTel(), //modded
+			'shopper.telephoneNumber'=> $_COOKIE["phonenumber"], //modded
 
             /** All server specific settings can be found in config/Config.php */
 
