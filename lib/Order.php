a12c6523decdee4e6097e93577e57d1a19015940
LAPTOP-D6G4E15I\User <kf@tamago.live>
2018-03-07 15:42:43 +0800

    revert

--- lib/Order.php
+++ lib/Order.php
@@ -63,7 +63,7 @@ class Order
 			$check = $this->amount['currency'];
 			if ($check == "VND" || $check == "IDR"){
 				$this->value = $this->value;
-				$this->amount['value'] = 26000000.00;
+				$this->amount['value'] = $this->value;
 			}else{
 				$this->value = $this->value*100;
 				$this->amount['value'] = $this->value;				
