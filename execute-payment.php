fce7d29ad623e45f4d24072b68a36085dda76713
NgKengFai <34302683+NgKengFai@users.noreply.github.com>
2018-04-14 21:44:04 +0800

    add missing token url

--- execute-payment.php
+++ execute-payment.php
@@ -10,7 +10,7 @@
 	//$payment_id = $_POST['payment']['id'];
 	$payment_id = $_POST['paymentID'];
 	$payer_id  = $_POST['payerID'];
-	echo "The post details".$payment_id.$payer_id;
+	//echo "The post details".$payment_id.$payer_id;
 	//Transaction Amount
 	//$_POST['payment']['transactions'][0]['amount']['currency'];
 
@@ -21,7 +21,8 @@
 	//$_POST['payment']['create_time'];
 
 	$execute_url = "https://api.sandbox.paypal.com/v1/payments/payment/".$payment_id."/execute/";	
-
+	$token_url = "https://api.sandbox.paypal.com/v1/oauth2/token";
+	
 	//API Credentials - Can Be Generated at https://developer.paypal.com/developer/applications/create
 	$clientID = "AXgrVs0H9QureJhIGNHrkTuQKWSnw3Yf0T82hi7DpiJpMAwbJ_8h6t-rAcasVZHiPC5J3X2DvekefJRq";
 	$clientSecret = "EG75KBlg7zFwUtmyyICwupK5TDk-GLSuLeJE2t5PZTTGaqAxkSuFn0T_Z56hhNeLqCwfGdg1FR1OtlBg";
