6d807b8e88d5ced4d72157a6d0113f4c8bdba2b1
NgKengFai <34302683+NgKengFai@users.noreply.github.com>
2018-04-16 23:28:21 +0800

    Revert "real live test 1"
    This reverts commit 8e955d8f5f7c7c394571088c279aa02b1516cd57.

--- execute-payment.php
+++ execute-payment.php
@@ -1,5 +1,5 @@
 <?php
-include "PayPalConfig.php";
+
 	//Transaction ID - Required For Refund
 	//$_POST['payment']['transactions'][0]['related_resources'][0]['sale']['id'];
 
@@ -10,26 +10,22 @@ include "PayPalConfig.php";
 	//$payment_id = $_POST['payment']['id'];
 	$payment_id = $_POST['paymentID'];
 	$payer_id  = $_POST['payerID'];
-	
 	//echo "The post details".$payment_id.$payer_id;
+	//Transaction Amount
+	//$_POST['payment']['transactions'][0]['amount']['currency'];
+
 	//Transaction Currency
-	$currency = $_POST['payment']['transactions'][0]['amount']['currency'];
+	//$_POST['payment']['transactions'][0]['amount']['total'];
 
-	//Transaction Value
-	$value = $_POST['payment']['transactions'][0]['amount']['total'];
-
-	//Description
-	$description =$_POST['payment']['transactions'][0]['description'];
 	//Transaction Time
 	//$_POST['payment']['create_time'];
 
-	//get url params	
 	$execute_url = "https://api.sandbox.paypal.com/v1/payments/payment/".$payment_id."/execute/";	
 	$token_url = "https://api.sandbox.paypal.com/v1/oauth2/token";
-
+	
 	//API Credentials - Can Be Generated at https://developer.paypal.com/developer/applications/create
-	//$clientID = "AXgrVs0H9QureJhIGNHrkTuQKWSnw3Yf0T82hi7DpiJpMAwbJ_8h6t-rAcasVZHiPC5J3X2DvekefJRq";
-	//$clientSecret = "EG75KBlg7zFwUtmyyICwupK5TDk-GLSuLeJE2t5PZTTGaqAxkSuFn0T_Z56hhNeLqCwfGdg1FR1OtlBg";
+	$clientID = "AXgrVs0H9QureJhIGNHrkTuQKWSnw3Yf0T82hi7DpiJpMAwbJ_8h6t-rAcasVZHiPC5J3X2DvekefJRq";
+	$clientSecret = "EG75KBlg7zFwUtmyyICwupK5TDk-GLSuLeJE2t5PZTTGaqAxkSuFn0T_Z56hhNeLqCwfGdg1FR1OtlBg";
 
 	/* Access Token Retrieval*/
 
@@ -58,8 +54,7 @@ include "PayPalConfig.php";
 
 	//body
 	$body = '{
-		"payer_id": "' . $payer_id . '",
-
+		"payer_id": "' . $payer_id . '"
 	}';
 
 
