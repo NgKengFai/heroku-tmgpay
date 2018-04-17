e2545ae2cd197380b7249dfd53bfc3ac6eef7b7b
NgKengFai <34302683+NgKengFai@users.noreply.github.com>
2018-04-17 15:19:45 +0800

    typo

--- execute-payment.php
+++ execute-payment.php
@@ -131,7 +131,7 @@ $response = json_decode($json, true);
 
 	if ($response['status'] == 'success'){
 		//$output = $response['message'];
-		header('Location: /payment-successful.php?transactionid='.$invoice.'&status=approved&tcoin='.$coin.'&money='.$currency.$money);
+		header('Location: /payment-successful.php?transactionid='.$invoice.'&status=approved&tcoin='.$coins.'&money='.$currency.$money);
 	}
 
 }else {
